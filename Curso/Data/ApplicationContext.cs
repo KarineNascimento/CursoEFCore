ec0f14bc8eda5d738517feedc07264ea20fbf9f2
KarineNascimento <karineyasmimsouza@live.com>
2021-10-05 01:07:54 -0300

    Logger e métodos de inserir dados na tabela

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,18 +1,22 @@
 ﻿using CursoEFCore.Data.Configurations;
 using CursoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CursoEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(k => k.AddConsole());
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<PedidoItem> PedidoItens { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Produto> Produtos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data source=NTM002\\NTM002; Initial Catalog=CursoEFCore; Integrated Security=true");
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .UseSqlServer("Data source=NTM002\\NTM002; Initial Catalog=CursoEFCore; Integrated Security=true");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- Curso/Program.cs
+++ Curso/Program.cs
@@ -12,11 +12,53 @@ namespace CursoEFCore
             //using var db = new Data.ApplicationContext();
             //db.Database.Migrations();
 
-            InserirDados();
+            //InserirDados();
+            //InserirDadosEmMassa();
         }
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "123456789123",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var lstClientes = new[]
+            {
+               new Cliente
+               {
+               Nome = "Karine Iasmim",
+                CEP = "99999999",
+                Cidade = "GRU",
+                Estado = "SP",
+                Telefone = "11999999999"
+               },
+               new Cliente
+               {
+               Nome = "Cliente Teste",
+                CEP = "0000000000",
+                Cidade = "GRU",
+                Estado = "SP",
+                Telefone = "1190000001"
+               }
+            };
+
+            using var db = new Data.ApplicationContext();
+            //db.AddRange(produto, cliente);
+            db.Produtos.Add(produto);
+            db.Clientes.AddRange(lstClientes);
+            var registros = db.SaveChanges();
+
+            Console.WriteLine($"Total de Registros(s): {registros}");
+        }
+
         private static void InserirDados()
         {
-            var produto = new Produto {
+            var produto = new Produto
+            {
                 Descricao = "Produto Teste",
                 CodigoBarras = "123456789123",
                 Valor = 10m,
